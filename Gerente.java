357580a26047c24376cff776d9624fad1faa1a3a
paulinasant <117967922+paulinasant@users.noreply.github.com>
2024-06-01 01:43:01 -0600

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,3 +1,43 @@
-class Gerente extends Persona {
-   
+import java.util.Date;
+
+public class Gerente extends Persona {
+    //private Sucursal sucursal;
+    private int numeroGerente;
+
+    public Gerente(String nombre, String paterno, String materno, Date fechaNacimiento, String curp,/* Sucursal sucursal,*/ int numeroGerente) {
+        super(nombre, paterno, materno, fechaNacimiento, curp);
+       /* this.sucursal = sucursal;*/
+        this.numeroGerente = numeroGerente;
+    }
+
+/*
+    public Sucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(Sucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+*/
+    public int getNumeroGerente() {
+        return numeroGerente;
+    }
+
+    public void setNumeroGerente(int numeroGerente) {
+        this.numeroGerente = numeroGerente;
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente" +
+                "nombre='" + getNombre() + '\'' +
+                ", paterno='" + getPaterno() + '\'' +
+                ", materno='" + getMaterno() + '\'' +
+                ", fechaNacimiento=" + getFechaNacimiento() +
+                ", curp='" + getCurp() + '\'' +
+                /*", sucursal=" + sucursal +*/
+                ", numeroGerente=" + numeroGerente;
+    }
 }
+//falta sucursal 
+//Crear el enum Sucursal
