c65b3e89d6d49a9db92893e93b5acea4bd606c45
paulinasant <117967922+paulinasant@users.noreply.github.com>
2024-06-01 01:23:09 -0600

    Update Veterinario.java

--- Veterinario.java
+++ Veterinario.java
@@ -1,8 +1,31 @@
 import java.util.*;
 
 class Veterinario extends Persona {
-
+    private int numeroCedula;
+     
+        public Veterinario(String nombre, String paterno, String materno, Date fechaNacimiento, String curp, int numeroCedula) {
+        super(nombre, paterno, materno, fechaNacimiento, curp);
+        this.numeroCedula = numeroCedula;
+    }
+    
+        public int getNumeroCedula() {
+        return numeroCedula;
+    }
+    
+        public void setNumeroCedula(int numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+    
+     @Override
+    public String toString() {
+        return "Veterinario" +
+                "nombre='" + getNombre() + '\'' +
+                ", paterno='" + getPaterno() + '\'' +
+                ", materno='" + getMaterno() + '\'' +
+                ", fechaNacimiento=" + getFechaNacimiento() +
+                ", curp='" + getCurp() + '\'' +
+                ", numeroCedula=" + numeroCedula ;
+    }
 }
-
 
 
